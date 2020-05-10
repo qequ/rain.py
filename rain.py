ea9f1083dacecb76c6cf995eb8bb26b29d6d69e3
qequ <alvarofriasgaray@gmail.com>
2020-05-10 13:28:55 -0300

    Added logic for main menu

--- rain.py
+++ rain.py
@@ -19,7 +19,60 @@ display_surface = pygame.display.set_mode((X, Y))
 
 font = pygame.font.Font('freesansbold.ttf', 20)
 
+#            new_tear = Tear(font, screen_width, color1, color2)
 
+main_menu = [Tear(font, X, green, blue, True, "rain.py"),
+             Tear(font, X, green, blue, True, "start"),
+             Tear(font, X, green, blue, True, "quit")
+             ]
+main_menu[0].x = X // 2
+main_menu[1].x = (X // 2) - 60
+main_menu[2].x = (X // 2) + 60
+
+r_main_menu = Rain(font, X, green, blue, main_menu, True)
+
+# start screen loop
+while len(r_main_menu.tears) == 3:
+
+    for t in r_main_menu.tears:
+        t.update_tear(font)
+
+    for t in main_menu:
+        if t.word == "rain.py" and t.y < (Y // 4):
+            t.update_y()
+
+        if (t.word == "start" or t.word == "quit") and t.y < ((Y // 4) + 50):
+            t.update_y()
+
+    display_surface.fill(white)
+
+    r_main_menu.render_tears(display_surface)
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            ch = pygame.key.name(event.key)
+            r_main_menu.lock_tear(ch)
+            r_main_menu.update_locked_tear(ch)
+            r_main_menu.remove_locked_tear()
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
+    sleep(0.001)
+
+exited = True
+
+for t in r_main_menu.tears:
+    if t.word == "quit":
+        exited = False
+
+if exited:
+    pygame.quit()
+    quit()
+
+# Main game loop
 r = Rain(font, X, green, blue)
 
 while True:
